da3859dbaab58a74a1604a127b9a10617c315119
Eggze2 <3088371460@qq.com>
2023-10-12 14:07:24 +0800

    倒数删除

---
+++ Notes/DA/getIntersectionNode.cpp
@@ -0,0 +1,14 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+class Solution {
+    public:
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        ListNode* curA = headA;
+        ListNode* curB = headB;
+        int lenA = 0, lenB = 0;
+        
+    }
+};
