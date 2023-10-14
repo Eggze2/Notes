53a59233de88d482692f913ad75f705605fa3be4
Eggze2 <3088371460@qq.com>
2023-10-14 16:25:19 +0800

    detectCircle

---
+++ Notes/DA/linkedList/detectCircle.cpp
@@ -0,0 +1,27 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+class Solution {
+    public:
+    ListNode* detectCircle(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if (slow == fast) {
+                ListNode* index1 = fast;
+                ListNode* index2 = head;
+                while (index1 != index2) {
+                    index1 = index1->next;
+                    index2 = index2->next;
+                }
+                return index2;
+            }
+        }
+        return nullptr;
+    }
+};
--- Notes/DA/linkedList/getIntersectionNode.cpp
+++ Notes/DA/linkedList/getIntersectionNode.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <utility>
 struct ListNode {
     int val;
     ListNode *next;
@@ -9,6 +11,37 @@ class Solution {
         ListNode* curA = headA;
         ListNode* curB = headB;
         int lenA = 0, lenB = 0;
-        
+        while (curA != nullptr) {
+            lenA++;
+            curA = curA->next;
+        }
+        while (curB != nullptr)
+        {
+            lenB++;
+            curB = curB->next;
+        }
+        curA = headA;
+        curB = headB;
+
+        if (lenB > lenA) {
+            std::swap(lenA, lenB);
+            std::swap(curA, curB);
+        }
+
+        int gap = lenA - lenB;
+
+        while (gap--)
+        {
+            curA = curA->next;
+        }
+
+        while (curA != nullptr) {
+            if (curA == curB) {
+                return curA;
+            }
+            curA = curA->next;
+            curB = curB->next;
+        }
+        return nullptr;
     }
 };
