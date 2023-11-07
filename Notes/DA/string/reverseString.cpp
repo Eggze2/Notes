776d21c2b7731bbca9858b707ded75c24d1b5faa
Eggze2 <3088371460@qq.com>
2023-11-07 16:49:59 +0800

    reverseString

---
+++ Notes/DA/string/reverseString.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <algorithm>
+class Solution {
+public:
+    void reverseString(std::vector<char>& s) {
+        for (int i = 0, j = s.size() - 1; i < s.size()/2; i++, j--) {
+            std::swap(s[i],s[j]);
+        }
+    }
+};
