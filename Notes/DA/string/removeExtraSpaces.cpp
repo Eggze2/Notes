6452601e202dff3e3c1a20f53513924cc0cb4176
Eggze2 <3088371460@qq.com>
2023-11-09 19:31:41 +0800

    removeExtraSpaces

--- Notes/DA/string/removeExtraSpaces.cpp
+++ Notes/DA/string/removeExtraSpaces.cpp
@@ -19,5 +19,34 @@
 #include <algorithm>
 class Solution {
 public:
-    
+    void reverse(std::string& s, int start, int end) {
+        for (int i = start, j = end; i < j; i++, j--) {
+            std::swap(s[i], s[j]);
+        }
+    }
+
+    void removeExtraSpaces(std::string& s) { // 去除所有空格并在相邻单词之间添加空格，快慢指针
+        int slow;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] != ' ') {
+                if (slow != 0) s[slow++] = ' ';
+                while (i < s.size() && s[i] != ' ') {
+                    s[slow++] = s[i++];
+                }
+            }
+        }
+        s.resize(slow);
+    }
+
+    std::string reverseWords(std::string s) {
+        removeExtraSpaces(s);
+        reverse(s, 0, s.size() - 1);
+        int start = 0;  // removeExtraSpace 后保证第一个单词的开始下标一定是 0
+        for (int i = 0; i <= s.size(); ++i) {
+            if (i == s.size() || s[i] == ' ') {
+                reverse(s, start, i - 1);
+                start = i + 1;
+            }
+        }
+    }
 };
